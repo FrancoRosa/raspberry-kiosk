17af350180e13da2a5b1d9f752cb1515f383d099
FrancoRosa <km115.franco@gmail.com>
2021-02-18 19:28:29 -0500

    Give update properties to build file

--- rpi/installer/update_logo.py
+++ rpi/installer/update_logo.py
@@ -1,4 +1,4 @@
-from os import listdir, system
+from os import listdir, system, popen
 from os.path import isfile, join, isdir
 import json
 
@@ -7,6 +7,31 @@ path = '../raspberry-kiosk/public/'
 path = 'build/'
 path = '/boot/build/'
 
-if 'logo.png' in listdir(path):
-  system("sudo cp /boot/build/logo.png /usr/share/plymouth/themes/pix/splash.png")
+def update_logo():
+  if 'logo.png' in listdir(path):
+    system("sudo cp /boot/build/logo.png /usr/share/plymouth/themes/pix/splash.png")
 
+def usb_available():
+    output = popen('lsblk').read()
+    return 'sda' in output
+
+def create_mount_folder():
+    system('sudo mkdir /media/pendrive')
+
+def mount_pendrive():
+    system('sudo mount -t auto /dev/sda1 /media/pendrive')
+
+def update_folder_present():
+    output = popen('ls /media/pendrive').read()
+    return 'update' in output
+
+def move_update():
+    system('sudo rm -r /boot/build');
+    system('sudo cp -r /media/pendrive/update/* /boot/');
+
+update_logo()
+if usb_available():
+    create_mount_folder()
+    mount_pendrive()
+    if update_folder_present():
+        move_update()
