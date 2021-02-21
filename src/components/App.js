e16819b2a562fcf81e05ac217201d918ce74d31c
FrancoRosa <km115.franco@gmail.com>
2021-02-21 10:47:03 -0500

    Add percentage value

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,22 @@ import Pdf from './Pdf';
 import Figure from './Figure';
 import Home from './Home';
 import List from './List';
+import BatteryStatus from './BatteryStatus';
 
 const App = () => {
   return (
-    <Switch>
-      <Route path="/vid/:id" component={Video} />
-      <Route path="/pdf/:id" component={Pdf} />
-      <Route path="/fig/:id" component={Figure} />
-      <Route path="/vids" component={() => <List category="vid" />} />
-      <Route path="/pdfs" component={() => <List category="pdf" />} />
-      <Route path="/figs" component={() => <List category="fig" />} />
-      <Route path="/" component={Home} />
-    </Switch>
+    <div>
+      <BatteryStatus />
+      <Switch>
+        <Route path="/vid/:id" component={Video} />
+        <Route path="/pdf/:id" component={Pdf} />
+        <Route path="/fig/:id" component={Figure} />
+        <Route path="/vids" component={() => <List category="vid" />} />
+        <Route path="/pdfs" component={() => <List category="pdf" />} />
+        <Route path="/figs" component={() => <List category="fig" />} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/components/BatteryStatus.jsx
@@ -0,0 +1,10 @@
+const BatteryStatus = () => {
+  return(
+    <div className="battery__status">
+      <img src="/icons/battery.png" alt="" srcset=""/>
+      <p>100</p>
+    </div>
+  )
+}
+
+export default BatteryStatus;
