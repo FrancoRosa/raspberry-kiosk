aa38ffb5bd3401587cee70be40f389e56d4b549a
FrancoRosa <km115.franco@gmail.com>
2021-03-14 23:24:41 -0500

    Add arrows to test buttons

--- src/components/ControlUsed.jsx
+++ src/components/ControlUsed.jsx
@@ -3,11 +3,20 @@ import { connect } from 'react-redux';
 
 const ControlUsed = ({ control }) => {
   
-  useEffect(()=>{console.log('XX>> ',control)},[])
+  useEffect(()=>{
+    console.log('XX>> ',control);
+    let container = document.querySelector('.control-used');
+    container.classList.remove('hidden');
+  },[])
 
   return(
     <p className="control-used">
-      {control}
+      {control === "a" && String.fromCodePoint(8592)}
+      {control === "d" && String.fromCodePoint(8594)}
+      {control === "w" && String.fromCodePoint(8593)}
+      {control === "s" && String.fromCodePoint(8595)}
+      {control === "o" && String.fromCodePoint(8617)}
+      {control === "p" && String.fromCodePoint(8618)}
     </p>
   )
 }
